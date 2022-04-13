4ac3e9f6e41e41f36e6f90ec40adc7248d2a4603
Oleg Bogomolov <bogomolovioleg88@gmail.com>
2022-04-13 14:17:51 +0300

    add itteration of string and checking for vowels

--- cw02.js
+++ cw02.js
@@ -1,2 +1,14 @@
 //We want to know the index of the vowels in a given word,
 // for example, there are two vowels in the word super (the second and fourth letters).
+function vowelIndices(word){
+    let indexArr = [];
+    let i = 1;
+    for (let chart of word.toLowerCase()){
+        if( chart == 'a' || 'e' || 'i' || 'o' || 'u' || 'y'){
+            indexArr.push(i++);
+        };
+        i++;
+    };
+    return indexArr;  
+
+}
